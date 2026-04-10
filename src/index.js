c18ef8e521ef04bb08c03bcbe6d479bd986571e2
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-04-09 20:51:30 -0700

    feat: successfully displayed the gif in the webpage using fetch() in a promise based operation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import "./styles.css";
+const apiKey = "IsYsjxtdkINxIUCj8uqTuZatf4yNuZwg";
+const input = document.getElementById("search");
+const button = document.querySelector(".btn");
+const image = document.querySelector(".image");
 
-console.log("Hello World!");
+button.addEventListener("click", () => {
+  const gif = input.value;
+
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${gif}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      console.log(response);
+      return response.json();
+    })
+    .then((result) => {
+      image.src = result.data.images.original.url;
+    })
+    .catch((error) => {
+      console.error("Error fetching the image:", error);
+    });
+});
